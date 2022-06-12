4b08d99725c56cc533a55288b24e33db3640f7b1
telekosmos <dar.es.balat@gmail.com>
2022-06-12 15:46:40 +0200

    feat: check for database and http errors

--- mobkoitask/main.py
+++ mobkoitask/main.py
@@ -1,4 +1,6 @@
+from sqlite3 import OperationalError
 import requests
+from psycopg2 import OperationalError, Error
 import db.postgres as pg
 from config import db_config, strategies
 
@@ -7,18 +9,32 @@ def get_api_data(url):
   return r
 
 def process_etl():
-  conn = pg.get_connection(db_config['host'], db_config['user'], db_config['password'], db_config['dbname'])
-  for strategy in strategies:
-    resp = get_api_data(strategy['url'])
-    print(f"got data {resp.status_code}")
-    storage_func = strategy['storage_func']
-    storage_func(strategy['output_folder'], strategy['output_filename'], resp.text)
-    print("saved to file")
-    rows = strategy['transform_func'](resp)
-    print(f"converted to rows {len(rows)}")
-    strategy['persist_func'](conn, rows)
-    print(f"persisted {len(rows)} rates")
+  def process_etl_strategies():
+    conn = pg.get_connection(db_config['host'], db_config['user'], db_config['password'], db_config['dbname'])
+    for strategy in strategies:
+      resp = get_api_data(strategy['url'])
+      if resp.status_code == 200:
+        print(f"got data {resp.status_code}")
+        storage_func = strategy['storage_func']
+        storage_func(strategy['output_folder'], strategy['output_filename'], resp.text)
+        print("saved to file")
+        rows = strategy['transform_func'](resp)
+        print(f"converted to rows {len(rows)}")
+        strategy['persist_func'](conn, rows)
+        print(f"persisted {len(rows)} rates")
+      else:
+        print(f'Error on request {strategy["url"]}: {resp.status_code}')
 
-  conn.close()
+    conn.close()
+
+  try:
+    process_etl_strategies()
+  except OperationalError:
+    print(f'Could not connect to database: {db_config}')
+  except Error as pg_ex:
+    print(f'Database error: {pg_ex.diag.message_detail}')
+  except requests.RequestException as req_ex:
+    print(f'HTTP request error - URL: {strategy["url"]}')
+
 
 process_etl()
