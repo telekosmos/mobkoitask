691ce649a89419c2a475aef4207cab69f205cda5
Guille <guillermo.c.martinez@dcsl.com>
2022-06-22 15:25:30 +0200

    chore: test request and db unhappy paths

--- tests/test_etl_process.py
+++ tests/test_etl_process.py
@@ -1,5 +1,7 @@
 
-import json
+import requests
+from psycopg2 import OperationalError
+
 from mobkoitask.config import etl_binance_config, db_config
 from mobkoitask.etl_process import run as process_etl
 from unittest import mock
@@ -51,18 +53,67 @@ def test_binance_etl(mock_makedirs, mock_file, mock_request, mock_pg_connect):
   """
   mock_request.return_value = MockResponse(200, binance_tickers_resp_fixture)
 
+  try:
+    strategies = [etl_binance_config]
+    process_etl(db_config, strategies)
+  finally:
+    mock_request.assert_called_with(etl_binance_config.url)
+    assert mock_file.call_count == 1
+    assert mock_pg_connect.call_count == 1
+    assert cur.execute.call_count == 5
+    assert cnxn.close.call_count == 1
+    assert mock_makedirs.call_count == 1
+
+@mock.patch("psycopg2.connect")
+@mock.patch("requests.get")
+@mock.patch("builtins.open", new_callable=mock.mock_open)
+@mock.patch("os.makedirs")
+def test_malformed_url(mock_makedirs, mock_file, mock_requests, mock_pg_connect):
+  mock_requests.side_effect = requests.RequestException()
   strategies = [etl_binance_config]
-  process_etl(db_config, strategies)
+  try:
+    process_etl(db_config, strategies)
+  finally:
+    assert mock_makedirs.call_count == 0
+    assert mock_file.call_count == 0
+    assert mock_pg_connect.call_count == 0
 
-  mock_request.assert_called_with(etl_binance_config.url)
-  assert mock_file.call_count == 1
-  assert mock_pg_connect.call_count == 1
-  assert cur.execute.call_count == 5
-  assert cnxn.close.call_count == 1
-  assert mock_makedirs.call_count == 1
+@mock.patch("psycopg2.connect")
+@mock.patch("requests.get")
+@mock.patch("builtins.open", new_callable=mock.mock_open)
+@mock.patch("os.makedirs")
+def test_db_no_connection(mock_makedirs, mock_file, mock_requests, mock_pg_connect):
+  mock_pg_connect.side_effect = OperationalError()
 
-def test_malformed_url():
-  pass
-
-def test_db_no_connection():
-  pass
+  binance_tickers_resp_fixture = """
+    [
+      {
+        "symbol": "ETHBTC",
+        "price": "0.05353600"
+      },
+      {
+        "symbol": "LTCBTC",
+        "price": "0.00182000"
+      },
+      {
+        "symbol": "BNBBTC",
+        "price": "0.00941200"
+      },
+      {
+        "symbol": "NEOBTC",
+        "price": "0.00039900"
+      },
+      {
+        "symbol": "QTUMETH",
+        "price": "0.00220900"
+      }]
+  """
+  mock_requests.return_value = MockResponse(200, binance_tickers_resp_fixture)
+  strategies = [etl_binance_config]
+  try:
+    process_etl(db_config, strategies)
+  finally:
+    assert mock_requests.call_count == 1
+    assert mock_pg_connect.call_count == 3 # retries
+    assert mock_makedirs.call_count == 0
+    assert mock_file.call_count == 0
